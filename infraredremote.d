d4de9fc4c52a456da0ff214aab51ae5f50e2c215
Benjamin Redl <benjaminredl@live.com>
2017-02-01 16:40:25 +0100

    Erstellung und erste Flankenerkennen
    Aufgertrene Probleme:
    Es werden nur etwa 17 Bits eingelesen und anschlißend nur 0er (eventuell
    die Start Bits der nächste Übertragungen?)
    
    Es können nicht mehere Tasten hintereinander eingelsen werden.
    Zählvariable die 32 Bit abzählt und anschließend die nächste Übertragung
    beginnt
    
    Zahlen unterscheiden sich nicht

---
+++ infraredremote.d
@@ -0,0 +1,7 @@
+infraredremote.o: infraredremote.c
+infraredremote.o: C:\Keil\ARM\INC\ST\STM32F10x\ARMV10_STD.h
+infraredremote.o: C:\Keil\ARM\INC\ST\STM32F10x\stm32f10x.h
+infraredremote.o: C:\Keil\ARM\RV31\INC\core_cm3.h
+infraredremote.o: C:\Keil\ARM\RV31\INC\stdint.h
+infraredremote.o: C:\Keil\ARM\INC\ST\STM32F10x\system_stm32f10x.h
+infraredremote.o: C:\Keil\ARM\RV31\INC\stdio.h
